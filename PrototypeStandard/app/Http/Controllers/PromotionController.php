f42d9326e98bb35e31ab4c592bb03e9c964c1c7c
alaeSouihli1 <114987252+alaeSouihli1@users.noreply.github.com>
2022-10-22 21:35:56 +0100

    prototype standard

---
+++ PrototypeStandard/app/Http/Controllers/PromotionController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Promotion;
+class PromotionController extends Controller
+{
+
+    public function index()
+    {
+        return view('promotions.index',['promotions' => Promotion::all()]);
+    }
+
+    public function create()
+    {
+        return view('promotions.create');
+    }
+
+
+    public function store(Request $request)
+    {
+        $promotion=new Promotion();
+        $promotion->name=$request->input('name');
+        $promotion->save();
+
+        return redirect()->route('promotions.index');
+    }
+
+
+    public function show($promotion)
+    {
+       return view('promotions.show',[ 'promotion' =>Promotion::findOrFail($promotion)]);
+    }
+
+
+    public function edit($promotion)
+    {
+        return view('promotions.edit',['promotion'=>Promotion::findOrFail($promotion) ]);
+    }
+
+
+    public function update(Request $request, $promotion)
+    {
+        $toUpdate=Promotion::findOrFail($promotion);
+        $toUpdate->name=strip_tags($request->input('name'));
+        $toUpdate->save();
+
+        return redirect()->route('promotions.index');
+    }
+
+
+    public function destroy($promotion)
+    {
+        $todelete=Promotion::findOrFail($promotion);
+        $todelete->delete();
+        return redirect(route('promotions.index'));
+    }
+}
