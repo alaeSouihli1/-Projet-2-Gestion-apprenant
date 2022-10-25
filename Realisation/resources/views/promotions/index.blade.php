98229e34f297ec225117289027c03270f660f07e
alaeSouihli1 <114987252+alaeSouihli1@users.noreply.github.com>
2022-10-25 16:45:10 +0100

    css

--- Realisation/resources/views/promotions/index.blade.php
+++ Realisation/resources/views/promotions/index.blade.php
@@ -8,44 +8,50 @@
     <title>Gestion des apprenants</title>
  </head>
  <body>
-    <h1>Liste des promotions</h1>
+    <!-- <h1>Solicode</h1> -->
+    <div class="container">
+        <div class="container-2">
+            <h1>Liste des promotions</h1>
+            <div class="search">
+                <input type="text" id="search" placeholder="Chercher promotion">
+            </div>
+    
+            <a href="{{route('promotions.create')}}">Create promotion</a>
 
-        <div class="search">
-            <input type="text" id="search" placeholder="Chercher promotion">
+            <table id="promotions">
+                <thead >
+                    <tr>
+                        <th>Id</th>
+                        <th>Nom</th>
+                        <th>fonctions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($promotions as $promotion)
+                        <tr>
+                            <td scope="row">{{ $promotion['id'] }}</td>
+                            <td> 
+
+                                <a href="{{route('promotions.show',['promotion'=>$promotion['id']])}}">
+                                    <li>{{ $promotion['name'] }} </li>
+                                </a>
+                            </td>
+                            <td>
+                                <a href="{{route('promotions.edit',$promotion['id'])}}">edit</a>
+                                <form action="{{route('promotions.destroy',$promotion->id)}}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <input  type="submit" value="Delete" />
+                                </form>
+                            </td>
+                        </tr>
+
+                    @endforeach
+                </tbody>
+            
+            </table>
         </div>
- 
- <a href="{{route('promotions.create')}}">Create promotion</a>
-
- <table>
-    <thead>
-        <tr>
-            <th>Promotion name</th>
-            <th>functions</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($promotions as $promotion)
-            <tr>
-                <td> 
-
-                    <a href="{{route('promotions.show',['promotion'=>$promotion['id']])}}">
-                        <li>{{ $promotion['name'] }} </li>
-                    </a>
-                </td>
-                <td>
-                    <a href="{{route('promotions.edit',$promotion['id'])}}">edit</a>
-                    <form action="{{route('promotions.destroy',$promotion->id)}}" method="POST">
-                        @csrf
-                        @method('DELETE')
-                        <input  type="submit" value="Delete" />
-                    </form>
-                </td>
-            </tr>
-
-        @endforeach
-    </tbody>
-  
-</table>
+    </div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 
